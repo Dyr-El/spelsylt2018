d6fed3db6196bff2b77ae96d84c35a7798dce011
Erik Dyrelius <erik.dyrelius@scania.com>
2018-11-16 15:36:48 +0100

    Not even start of forthleft.

--- tutorial/forthleft/parse.lua
+++ tutorial/forthleft/parse.lua
@@ -1,2 +1,18 @@
-function skipSpaces(memory) 
+
+
+
+function skipSpaces(inp)
+    if inp:empty() then
+        inp:refill()
+    end
+    while not inp:empty() do
+        if isWhitespace(inp:next()) then
+            inp:step()
+        else
+            break
+        end
+    end
+end
+
+
     
