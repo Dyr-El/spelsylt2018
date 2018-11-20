dbd61d66a958cf9ae5214b47f37bc69e7d956cc3
Erik Dyrelius <erik.dyrelius@gmail.com>
2018-11-20 08:58:55 +0100

    A teeny bit of parser implemented

--- tutorial/forthleft/parse.lua
+++ tutorial/forthleft/parse.lua
@@ -7,6 +7,49 @@
 -- A character aligend address is the same as an address
 -- An aligned address is and address with the two least significant bits set to 0
 
+InputStream = {buffer = "", idx = 1}
+InputStream.new = function(self, object)
+    object = object or {}
+    setmetatable(object, self)
+    self.__index = self
+    return object
+end
+
+InputStream.empty = function(self)
+    return #(self.buffer) < self.idx
+end
+
+InputStream.refill = function(self)
+    -- TODO: Really inefficient!
+    self.buffer = self.buffer .. io.read()
+end
+
+InputStream.next = function(self)
+    if self:empty() then
+        return ""
+    else
+        return string.sub(self.buffer, self.idx, self.idx)
+    end
+end
+
+InputStream.consume = function(self, count)
+    count = count or 1
+    self.idx = self.idx + count
+end
+
+InputStream.print = function (self)
+    io.write("[")
+    io.write(self.buffer)
+    io.write("]\n ")
+    for i=1, self.idx-1 do
+        io.write(" ")
+    end
+    io.write("^\n")
+end
+
+function isWhitespace(c)
+    return c:find("%s") == 1
+end
 
 function skipSpaces(inp)
     if inp:empty() then
@@ -14,12 +57,17 @@ function skipSpaces(inp)
     end
     while not inp:empty() do
         if isWhitespace(inp:next()) then
-            inp:step()
+            inp:consume(1)
         else
             break
         end
     end
 end
 
-
-    
+myStream = InputStream:new()
+skipSpaces(myStream)
+while not myStream:empty() do
+    io.write(myStream:next())
+    myStream:consume()
+end
+print()
